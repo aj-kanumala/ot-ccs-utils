9f3fc2313b638fa713c0bd67eaa76223db6e3d3b
Ajay Kanumala <axk8440@mavs.uta.edu>
2026-03-23 14:24:47 -0500

    Initial library setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ot-ccs-utils",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas",
+        "matplotlib",
+    ],
+    python_requires=">=3.8",
+)
